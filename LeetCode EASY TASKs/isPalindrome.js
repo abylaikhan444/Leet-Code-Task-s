cf8f25f99e44b53db5e0ef4af39ff59d0b6f2bbd
abylaikhan444 <fifa77882@gmail.com>
2026-07-12 18:58:14 +0500

    Complate the task of [Is Polindrome]

---
+++ LeetCode EASY TASKs/isPalindrome.js
@@ -0,0 +1,20 @@
+function isPalindrome(str) {
+  str = str.toString().toUpperCase();
+
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    } else {
+      left = left + 1;
+      right = right - 1;
+    }
+  }
+  return true;
+}
+console.log(isPalindrome("Мадам"));
+console.log(isPalindrome("Стол"));
+console.log(isPalindrome(121));
+console.log(isPalindrome(1212));
--- LeetCode EASY TASKs/test.js
+++ LeetCode EASY TASKs/test.js
@@ -1,10 +1,13 @@
-const obj = {
-  "(": ")",
-  "[": "]",
-  "{": "}",
-};
+// const obj = {
+//   "(": ")",
+//   "[": "]",
+//   "{": "}",
+// };
 
-const stack = ["{"];
-const open = stack.pop();
+// const stack = ["{"];
+// const open = stack.pop();
 
-console.log(obj[open]);
+// console.log(obj[open]);
+
+const url = URL.parse("https://dev-station.ru/problems");
+console.log(url.hostname);
